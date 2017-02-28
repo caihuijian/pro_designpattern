64b960dff738c1433186d210b6d1c26d4b6826e1
caihuijian <1522649108@qq.com>
2017-02-28 15:54:29 +0800

    add for prototype

---
+++ Prototype Pattern FromNet/src/client/Client.java
@@ -0,0 +1,13 @@
+package client;
+
+import prototype.ConcretePrototype;
+
+public class Client {    
+    public static void main(String[] args){    
+        ConcretePrototype cp = new ConcretePrototype();    
+        for(int i=0; i< 10; i++){    
+            ConcretePrototype clonecp = (ConcretePrototype)cp.clone();    
+            clonecp.show();    
+        }    
+    }    
+}
